10e4eaa4896f871fb11e0b53263027a2254f54ed
Дмитрий <shcheklein.dev@gmail.com>
2024-08-03 18:02:31 +0300

    fix: layout

--- js/components/languages/languages.js
+++ js/components/languages/languages.js
@@ -2,23 +2,26 @@ import { Box } from './../box/box';
 import './languages.css';
 
 export function Languages() {
+  const data = [
+    { title: 'English', value: 100 },
+    { title: 'Malayalam', value: 100 },
+    { title: 'Hindi', value: 80 },
+  ];
+
   return Box({
     className: 'box--languages',
     title: 'Languages',
     children: /*html*/ `
         <ul class="languages-list">
-            <li class="languages-list__item">
-                <h3 class="languages-list__title">English</h3>
-                <progress class="languages-list__progress" value="90" max="100">90%</progress>
-            </li>
-            <li class="languages-list__item">               
-                <h3 class="languages-list__title">Malayalam</h3>                
-                <progress class="languages-list__progress" value="80" max="100">80%</progress>                
-            </li>
-            <li class="languages-list__item">               
-                <h3 class="languages-list__title">Hindi</h3>                
-                <progress class="languages-list__progress" value="80" max="100">80%</progress>                
-            </li>
+            ${data
+              .map(
+                ({ title, value }) => /*html*/ `
+                <li class="languages-list__item">
+                    <h3 class="languages-list__title">${title}</h3>
+                    <progress class="languages-list__progress" value="${value}" max="100">${value}%</progress>
+                </li>`,
+              )
+              .join('')}
         </ul>
         `,
   });
